0137151e0cbd3ca6b025bdc77614720ea6d25b0b
changujung <chan999k@gmail.com>
2021-01-17 11:18:46 +0900

    week08

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
-from flask import Flask, render_template
+from pymongo import MongoClient
+
+from flask import Flask, render_template, jsonify, request
 
 app = Flask(__name__)
+
+client = MongoClient('localhost', 27017)
+db = client.my_project
 
 
 @app.route('/')
@@ -11,6 +16,20 @@ def home():  # 함수명 수정 - 이름만 보고 접속되는 페이지를 확
 @app.route('/mypage')
 def my_page():
     return 'This is My Page!'
+
+
+@app.route('/star', methods=['POST'])
+def star_point():
+    star_receive = request.form['give_star']
+    name_receive = request.form['give_name']
+    db.WOW.insert_one({'이름': name_receive, '평점': star_receive})
+    return jsonify({'result': 'success'})
+
+
+@app.route('/star', methods=['GET'])
+def death_tanker_count():
+    all_Count = db.WOW.count({'이름': '죽음의 상흔 탱커'})
+    return jsonify({'result': 'success'})
 
 
 if __name__ == '__main__':
