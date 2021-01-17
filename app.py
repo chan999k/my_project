4a37594ef40c41ae9b966d40d741a69a3ad146b7
changujung <chan999k@gmail.com>
2021-01-17 16:01:31 +0900

    finalweek

--- app.py
+++ app.py
@@ -25,12 +25,26 @@ def star_point():
     db.WOW.insert_one({'이름': name_receive, '평점': star_receive})
     return jsonify({'result': 'success'})
 
+# 실패작 참고용
+# def death_tanker_count():
+#     all_count = db.WOW.count_document({'이름': '죽음의 상흔 탱커'})
+#     stars = list(db.WOW.find({'이름': '죽음의 상흔 탱커'}))
+#     print(stars['평점'])
+#     for all_star in stars:
+#         basic_star = 0
+#         all_star = all_star + basic_star
+#         print(all_star)
+#     return jsonify({'result': 'success'})
 
 @app.route('/star', methods=['GET'])
 def death_tanker_count():
-    all_Count = db.WOW.count({'이름': '죽음의 상흔 탱커'})
-    return jsonify({'result': 'success'})
-
+    stars = list(db.WOW.find({'이름': '죽음의 상흔 탱커'}))
+    all_count = len(stars)
+    sum = 0
+    for star in stars:
+        num = int(star['평점'])
+        sum = sum + num
+    return jsonify({'result': 'success', '개수': all_count, '합': sum})
 
 if __name__ == '__main__':
     app.run('127.0.0.1', port=5001, debug=True)
